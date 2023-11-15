dda425025c6cc81fff06e33a0fb6ad2ab5c16df6
joeCodes05 <nathancodes05@gmail.com>
2023-11-15 15:22:37 +0100

    project init - env setup, created branch.

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <>
+        <aside className="fixed w-[5%] left-0 top-0 bottom-0 h-full bg-red-500">
+
+        </aside>
+    </>
+  )
+}
+
+export default Sidebar
