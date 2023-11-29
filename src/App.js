963f11994c9dab3d24e9162ec9fcce071fd9f9f9
Nathaniel Joseph <145021302+joeCodes05@users.noreply.github.com>
2023-11-29 22:22:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
+import Sports from "./pages/sports/Sports";
+import Transfers from "./pages/transfers/Transfers";
+import News from "./pages/news/News"
+import Competitions from "./pages/competitions/Competitions";
+import Team from "./pages/teams/Team";
+import About from "./pages/about/About";
+import Live from "./pages/live/Live";
 import Sidebar from "./components/sidebar";
 
 const App = () => {
@@ -8,9 +16,18 @@ const App = () => {
     <>
       <Router>
         <Sidebar />
-        <Routes>
-          <Route exact element={<Home />} path="/" />
-        </Routes>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sports" element={<Sports />} />
+            <Route path="/transfers" element={<Transfers />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/competitions" element={<Competitions />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/live" element={<Live />} />
+            <Route path="/login" element={<Live />} />
+          </Routes>
       </Router>
     </>
   );
