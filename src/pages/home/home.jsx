235e8847c790f629bfd0ee23f880ffdec70e9f4f
joeCodes05 <nathancodes05@gmail.com>
2023-11-30 00:02:28 +0100

    cleared exsiting code

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="bg-indigo flex items-center justify-center">
-        <h2 className="text-white">HOME</h2>
-      </div>
+      
     </>
   );
 };
