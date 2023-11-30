3c9d07a74e4d9622e67fb3e77c3a0362de906f89
joeCodes05 <nathancodes05@gmail.com>
2023-11-30 19:37:55 +0100

    minor code updates

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <>
-      <h1 className='text-white font-bold text-2xl'><span className='text-primary'>Go</span>Ball</h1>
+      <h1 className='text-white font-bold text-2xl' aria-label="Go Ball brand"><span className='text-primary'>Go</span>Ball</h1>
     </>
   );
 };
--- src/utils/button.jsx
+++ src/utils/button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({ variant, text }) => {
+const Button = ({ variant, text, role, ariaLabel, onClick }) => {
   // the variant prop should only reseive boolean values
   // "True" representing the button with outline and transparent background by default 
   // "False" representing the button with no outline and a solid background by default 
@@ -8,10 +8,10 @@ const Button = ({ variant, text }) => {
   return (
     <>
       {variant ?
-        <button className='py-1.5 px-5 rounded-md ring-1 ring-primary text-primary bg-transparent duration-500'>
+        <button onClick={onClick} role={role} aria-label={ariaLabel} className='py-1.5 px-5 rounded-md ring-1 ring-primary text-primary bg-transparent duration-500 outline-none'>
           {text}
         </button> :
-        <button className='py-1.5 px-5 rounded-md ring-1 ring-primary text-darkBg bg-primary duration-500'>
+        <button onClick={onClick} role={role} aria-label={ariaLabel} className='py-1.5 px-5 rounded-md ring-1 ring-primary text-darkBg bg-primary duration-500 outline-none'>
           {text}
         </button>}
     </>
